7727a71cf31d707d284304a4bd4f9af74e1905ec
赫英年 <heyingnian@zving.com>
2025-10-31 12:43:22 +0800

    环境搭建：微服务架构基础环境配置完成

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 /**
  * API网关启动类
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.apigateway", "com.example.common"}, exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class, org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class})
 @EnableDiscoveryClient
 @RefreshScope
 public class ApiGatewayApplication {
